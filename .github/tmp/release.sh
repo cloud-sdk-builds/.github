172a2129f898075625d84cc52d4786431b009630
Vaddiraju Surya Teja <112264612+suryavaddiraju@users.noreply.github.com>
2026-02-06 05:44:53 +0530

    test

--- .github/tmp/release.sh
+++ .github/tmp/release.sh
@@ -8,4 +8,4 @@ node ./makefiles.mjs
 git add :/.github/README.md :/index.min.mjs :/.github/tmp/LAST_VERSION.txt
 git commit -S -m "chore: update to $REPO_VERSION"
 git push origin HEAD
-gh release create "$REPO_VERSION" --repo "${ORG_NAME}/${REPO_NAME}" --title "$REPO_VERSION" --notes-file :/release.md
+gh release create "$REPO_VERSION" --repo "${ORG_NAME}/${REPO_NAME}" --title "$REPO_VERSION" --notes-file ./release.md
